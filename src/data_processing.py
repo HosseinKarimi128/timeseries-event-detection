1974ec88e2ef4e115b0978bbb657affda558e6f6
hosseinkarimi128 <hossein.karimi.0128@gmail.com>
2025-01-19 19:45:19 +0330

    debugging

--- src/data_processing.py
+++ src/data_processing.py
@@ -66,7 +66,7 @@ def create_features_tensor(_paths, max_len):
     
     # Stack all features into a single numpy array before converting to tensor
     _features = np.stack(_features, axis=-1)
-    _original_time_series = _original_time_series.values
+    # _original_time_series = _original_time_series.values
     return torch.tensor(_features, dtype=torch.float32), torch.tensor(_original_time_series, dtype=torch.float32)
 
 def create_labels_tensor(_paths, max_len):
